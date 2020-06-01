9749694317ff5bfb0424c5aeadf41bc77bde0967
Walber Nunes <walber026@gmail.com>
2020-06-01 15:31:01 -0400

    Kadene's algorithm

--- algoriths/kadene.py
+++ algoriths/kadene.py
@@ -1,20 +1,21 @@
 
-def Kadene(arr):
+def kadene(arr):
     max_glob = arr[0]
     max_curr = arr[0]
     bounds = (0, 1)
+    lb = bounds[0]
 
     for i in range(1, len(arr)): 
         if arr[i] >= (max_curr + arr[i]):
             max_curr = arr[i]
-            new_bounds = (i, i + 1)
+            lb = i 
         else:
-            max_curr = max_curr + arr[i] 
-            new_bounds = (bounds[0], i + 1)
+            max_curr = max_curr + arr[i]
+            lb = bounds[0]
 
         if max_curr > max_glob:
             max_glob = max_curr
-            bounds = new_bounds
+            bounds = (lb, i + 1)
 
     lb, ub = bounds
     return arr[lb:ub]
