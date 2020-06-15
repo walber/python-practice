144299234078fa18402f0e59e0f10c78b2915955
Walber Nunes <walber026@gmail.com>
2020-06-14 22:07:45 -0400

    Iterative Quicksort

--- algorithms/quicksort.py
+++ algorithms/quicksort.py
@@ -18,11 +18,23 @@ def quicksort(arr):
 
         return l
 
-
+    """
     def sort(arr, lb, ub):
         if lb < ub:
             i = partition(arr, lb, ub)
             sort(arr, lb, i - 1)
             sort(arr, i, ub)
+    """
+    def sort(arr, lb, ub):
+        stk = [(lb, ub)]
+
+        while stk:
+            l, r = stk.pop()
+
+            if l < r:
+                i = partition(arr, l, r)
+                stk.append((l, i - 1))
+                stk.append((i, r))
 
     return sort(arr, 0, len(arr) - 1)
+
